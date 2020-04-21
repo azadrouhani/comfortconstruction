3329a3e096030bb0d147b834a851bbb8a9e11902
azadrouhani <42624188+azadrouhani@users.noreply.github.com>
2020-04-21 11:56:03 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).foundation()
+document.getElementById("email").innerHTML = "comfortcoco@yahoo.com";
+document.getElementById("phone").innerHTML = "925-575-0626";
+document.getElementById("address").innerHTML = "2700 Cherry Lane, Walnut Creek, CA, 94597";
+document.getElementById("phonecontent1").innerHTML = "Email: comfortcoco@yahoo.com";
+document.getElementById("phonecontent2").innerHTML = "Phone Number: 925-575-0626";
+document.getElementById("phonecontent3").innerHTML = "Address: 2700 Cherry Lane, Walnut Creek, CA, 94597";
+
+document.getElementById("submitbutton").style.cursor = "pointer";
